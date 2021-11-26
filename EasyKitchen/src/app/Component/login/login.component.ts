a4a4dcca76d1cf0281fd9206c2b4b427b2be2231
RosaMedina28 <rosamedina1008@gmail.com>
2021-11-26 09:53:47 -0600

    qwwqw

--- EasyKitchen/src/app/Component/login/login.component.ts
+++ EasyKitchen/src/app/Component/login/login.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+export class LoginComponent implements OnInit{
+  email: String;
+  password: String;
+  nombre: String
+  request: object;
+
+  loginForm:FormGroup; //Variable para validar forms
+  
+  constructor(private api: ApiService,private fb:FormBuilder,public router: Router,private cookies: CookieService) {
+    this.logForm();
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  /* Form validation and setting */
+  setLogin() {
+    this.email = this.loginForm.get('email').value
+    this.password = this.loginForm.get('password').value
+    }
+
+  logForm(): void {
+    this.loginForm = this.fb.group({
+      email: ['',[Validators.required]],
+      password: ['',[Validators.required]]
+    });
+  }
+  /* Form validation and setting */
+
+  login() {
+     this.api.login(this.request).subscribe(data => {
+      
+    }, error => {
+     
+      
+    });
   }
 
 }
